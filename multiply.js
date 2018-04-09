7f5df2e811f5a037d6676268dace33fcb01776fb
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-04-09 15:59:36 -0400

    Success in reading two numbers, so proud

--- multiply.js
+++ multiply.js
@@ -1,5 +1,9 @@
 // read a couple of numbers from console
 
+num1 = process.argv[2]
+num2 = process.argv[3]
+
+console.log("Read two parameters: ", num1, num2)
 // calculate the multiplication
 
 // print the results
